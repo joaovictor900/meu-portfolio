b2d43b617bdd80fcd7699fa1c0fdc297f9da3184
joaovictor900 <victorsantos900@gmail.com>
2020-01-14 22:23:56 -0300

    Commit inicial do meu portifólio

---
+++ src/test/java/br/com/meuportifolio/MeuPortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuportifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
